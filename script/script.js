f327e50342d6b2faba018747bff7f031a877d477
IT <it.corvo@firefoxbd.com>
2025-03-01 15:12:15 +0600

    javascript applied

--- script/script.js
+++ script/script.js
@@ -2,12 +2,61 @@ const dates=new Date();
 const hours=dates.getHours();
 const minutes=dates.getMinutes();
 const seconds=dates.getSeconds();
-const times= if(hours>12)?(hours-12):hours +':'minutes+':'+seconds;
+const hourss=((hours>12)?(hours-12):hours);
+const times= ((hourss<10)?('0'+hourss):hourss) + ':' + ((minutes<10)?('0'+minutes):minutes) + ':' + ((seconds<10)?('0'+seconds):seconds) + ' ' + ((hours<12)?'AM':'PM');
 
 document.getElementById('task-1').addEventListener('click',
 function(event){
         event.preventDefault;
-        const title=document.getElementById('task-1-name').value;
+        const title=document.getElementById('task-1-name').innerText;
+        alert('You have Complete The Task '+ title +' at '+times);
+    }
+)
+
+document.getElementById('task-1').addEventListener('click',
+    function(event){
+        event.preventDefault;
+        const title=document.getElementById('task-1-name').innerText;
+        alert('You have Complete The Task '+ title +' at '+times);
+    }
+)
+
+document.getElementById('task-1').addEventListener('click',
+    function(event){
+        event.preventDefault;
+        const title=document.getElementById('task-1-name').innerText;
+        alert('You have Complete The Task '+ title +' at '+times);
+    }
+)
+
+document.getElementById('task-1').addEventListener('click',
+    function(event){
+        event.preventDefault;
+        const title=document.getElementById('task-1-name').innerText;
+        alert('You have Complete The Task '+ title +' at '+times);
+    }
+)
+
+document.getElementById('task-1').addEventListener('click',
+    function(event){
+        event.preventDefault;
+        const title=document.getElementById('task-1-name').innerText;
+        alert('You have Complete The Task '+ title +' at '+times);
+    }
+)
+
+document.getElementById('task-1').addEventListener('click',
+    function(event){
+        event.preventDefault;
+        const title=document.getElementById('task-1-name').innerText;
+        alert('You have Complete The Task '+ title +' at '+times);
+    }
+)
+
+document.getElementById('task-6').addEventListener('click',
+    function(event){
+        event.preventDefault;
+        const title=document.getElementById('task-1-name').innerText;
         alert('You have Complete The Task '+ title +' at '+times);
     }
 )
